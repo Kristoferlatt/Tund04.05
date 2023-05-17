a156aab1a3c1abd9b722c7b3e725e75a18d4fc3f
Kristoferlatt <42769298+Kristoferlatt@users.noreply.github.com>
2023-05-17 17:29:08 +0300

    part6

--- angular-personal-dasboard/src/app/app.component.ts
+++ angular-personal-dasboard/src/app/app.component.ts
@@ -1,10 +1,34 @@
+import { animate, animation, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations:[
+    trigger("routeAnim", [
+      transition("* => *", [
+        query(':enter', [
+        style({
+        background: 'wheat'
+        })
+      ]),
+        style({
+          background: "blue"
+        }),
+        animate(1000)
+      ])
+
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-personal-dasboard';
+  prepareRoute(outlet: RouterOutlet) {
+    if (outlet.isActivated) {
+      const tab = outlet.activatedRouteData['tab']
+      if (!tab) return 'secondary'
+      return tab
+    }
+  }
 }
