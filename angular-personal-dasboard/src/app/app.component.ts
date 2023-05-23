3f7fb3783a7910926dfdd271d2e8d29704153b2c
Kristoferlatt <42769298+Kristoferlatt@users.noreply.github.com>
2023-05-23 13:30:08 +0300

    part7

--- angular-personal-dasboard/src/app/app.component.ts
+++ angular-personal-dasboard/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { animate, animation, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { animate, animation, group, query, style, transition, trigger } from '@angular/animations';
+import { Component, Optional } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,16 +8,83 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   animations:[
     trigger("routeAnim", [
-      transition("* => *", [
-        query(':enter', [
+      transition(':increment', [
         style({
-        background: 'wheat'
-        })
-      ]),
-        style({
-          background: "blue"
+          position: 'relative',
+          overflow: 'hidden'
         }),
-        animate(1000)
+
+        query(' :enter, :leave',[
+          style({ 
+            //display: 'block',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height:'100%'
+          })
+        ], { optional: true}),
+
+        group([
+          query(' :leave', [
+            animate('200ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(-50px)'
+            }))
+          ], {optional: true}),
+          query(' :enter', [
+            style({
+              transform: 'translateX(50px)',
+              opacity:0
+  
+            }),
+            animate('250ms 120ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            }))
+          ], {optional: true})
+
+        ])
+          
+      ]),
+      transition(':decrement', [
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+
+        query(' :enter, :leave',[
+          style({ 
+            //display: 'block',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height:'100%'
+          })
+        ], { optional: true}),
+
+        group([
+          query(' :leave', [
+            animate('200ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(50px)'
+            }))
+          ], {optional: true}),
+          query(' :enter', [
+            style({
+              transform: 'translateX(-50px)',
+              opacity:0
+  
+            }),
+            animate('250ms 120ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            }))
+          ], {optional: true})
+
+        ])
+          
       ])
 
     ])
