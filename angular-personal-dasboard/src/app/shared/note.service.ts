892c5ed179ca5731c54478068f10076c92c937d3
Kristoferlatt <42769298+Kristoferlatt@users.noreply.github.com>
2023-05-24 17:41:13 +0300

    part8

--- angular-personal-dasboard/src/app/shared/note.service.ts
+++ angular-personal-dasboard/src/app/shared/note.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Note } from './note.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
+  notes: Note[]
+
   constructor() { }
+
+  getNotes(){
+    return this.notes
+  }
+
+  getNote(id: string){
+    return this.notes.find(n => n.id === id)
+  }
+  addNote(note: Note){
+    this.notes.push(note)
+  }
+  updateNote(id: string, updatedFields: Partial<Note>){
+    const note = this.getNote(id)
+    Object.assign(note, updatedFields)
+
+  }
+  
 }
