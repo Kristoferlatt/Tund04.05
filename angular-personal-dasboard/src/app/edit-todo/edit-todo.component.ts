9c48e84d221ea4ff231ce718ceaae7bc465c5e34
Kristoferlatt <kristoferlatt09@gmail.com>
2023-05-31 05:33:19 -0700

    part18

--- angular-personal-dasboard/src/app/edit-todo/edit-todo.component.ts
+++ angular-personal-dasboard/src/app/edit-todo/edit-todo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { NotificationService } from '../shared/notification.service';
+import { Todo } from '../shared/todo.model';
+import { TodoService } from '../shared/todo.service';
 
 @Component({
   selector: 'app-edit-todo',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditTodoComponent implements OnInit {
 
-  constructor() { }
+  todo!: Todo | undefined
+  
+  constructor(
+    private route: ActivatedRoute,
+    private todoService: TodoService,
+    private router: Router,
+    private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      const todoId = paramMap.get('id')
+      if (todoId !== null) {
+        this.todo = this.todoService.getTodo(todoId);
+      }
+    })
   }
 
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) return
+    
+    this.todoService.updateTodo(this.todo!.id, form.value)
+    this.router.navigateByUrl("/todos")
+    this.notificationService.show('Todo updated!')
+  }
+  
 }
